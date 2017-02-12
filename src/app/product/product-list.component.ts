6767f3f78ca4b6444579394f64b24ebad9bc48a2
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-02-12 16:49:27 -0600

    formatting

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -16,61 +16,62 @@ export class ProductListComponent implements OnInit {
     perPageCnt = 10;
     orderBy = 'productName';
     prod_cats = [
-                ['all', 'All'],
-                ['SweetMini', 'Sweet Minis'],
-                ['Sweet9', 'Sweet 9"'],
-                ['Sweet10', 'Sweet 10"'],
-                ['Sweet8', 'Sweet 8"'],
-                ['Sweet6', 'Sweet 6"']
-                ];
+        ['all', 'All'],
+        ['SweetMini', 'Sweet Minis'],
+        ['Sweet9', 'Sweet 9"'],
+        ['Sweet10', 'Sweet 10"'],
+        ['Sweet8', 'Sweet 8"'],
+        ['Sweet6', 'Sweet 6"']
+    ];
     order_cats = [
         ['productName', 'Product Name'],
         ['productType', 'Product Type'],
         ['priceAscending', 'Price Ascending'],
         ['priceDescending', 'Price Descending']
-        ];
+    ];
     per_page_options = [10, 20, 40];
 
     constructor(private _productService: ProductService) {
+
     }
-    ngOnInit(){
+    ngOnInit() {
         console.log('Calling getProducts');
         this._productService.getProductPage()
-        .subscribe(
+            .subscribe(
             page => this.page = page,
             error => this.errMessage = <any>error
-        );
+            );
     }
 
-    next(page: number, psize: number, orderBy: string, category: string){
+    next(page: number, psize: number, orderBy: string, category: string) {
         this._productService.getProductPage(page, psize | this.perPageCnt, this.orderBy || orderBy, this.selectedCategory || category)
-        .subscribe(
+            .subscribe(
             page => this.page = page,
             error => this.errMessage = <any>error
-        );
+            );
     }
 
-    canGoBack(){
+    canGoBack() {
         return !(this.page.currentPage > 1);
     }
 
-    canGoForward(){
+    canGoForward() {
         return !(this.page.currentPage < this.page.totalPages);
     }
 
-    categoryChanged(newValue: string){
+    categoryChanged(newValue: string) {
         // console.log('New Category: ' + newValue);
         // this.selectedCategory = newValue;
         this.next(1, this.perPageCnt, this.orderBy, this.selectedCategory);
     }
 
-    pageCountChanged(newValue: number){
+    pageCountChanged(newValue: number) {
         console.log('New Page Count: ' + newValue);
         this.perPageCnt = +newValue;
         this.next(1, this.perPageCnt, this.orderBy, this.selectedCategory);
     }
 
-    orderByChanged(newValue: string){
+    orderByChanged(newValue: string) {
         console.log('New Order By: ' + newValue);
         this.orderBy = newValue;
         this.next(1, this.perPageCnt, this.orderBy, this.selectedCategory);
