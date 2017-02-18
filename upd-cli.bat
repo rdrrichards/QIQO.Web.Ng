c4584be10109e32dbd40f328cbc9622462a59261
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-02-17 20:14:03 -0600

    dealing with cli bs

---
+++ upd-cli.bat
@@ -0,0 +1,10 @@
+call npm uninstall -g angular-cli @angular/cli
+call npm cache clean
+call npm install -g @angular/cli@latest
+
+rmdir node_modules /s /q
+rmdir dist /s /q
+rmdir tmp /s /q
+
+call npm install --save-dev @angular/cli@latest
+call npm install
