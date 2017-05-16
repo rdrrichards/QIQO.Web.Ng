a24af4bbd086d1ac04342602c5900660a4ec72ea
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-05-16 17:41:21 -0500

    cli 1.1.0-rc.0 update

--- upd-cli.bat
+++ upd-cli.bat
@@ -1,10 +1,10 @@
 call npm uninstall -g @angular/cli
 call npm cache clean
-call npm install -g @angular/cli@latest
+call npm install -g @angular/cli@1.1.0-rc.0
 
 rmdir node_modules /s /q
 rmdir dist /s /q
 rmdir tmp /s /q
 
-call npm install --save-dev @angular/cli@latest
+call npm install --save-dev @angular/cli@1.1.0-rc.0
 call npm install
