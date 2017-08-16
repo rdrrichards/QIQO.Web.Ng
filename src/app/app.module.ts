f4563c1e68f9220d7b85ff005205498bdc25980d
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-08-16 17:25:10 -0500

    removed HttpModule ref

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductModule } from './product/product.module';
 import { AccountModule } from './account/account.module';
@@ -28,7 +27,6 @@ import { AuthService } from './auth/auth.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     HttpClientModule,
     AccountModule,
     ProductModule,
