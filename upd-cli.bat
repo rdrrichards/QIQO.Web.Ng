317816ab01cb0c02e854ed5d3905a5604e1991d6
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2018-09-08 12:51:08 -0500

    ng/cli updates 6.x

--- upd-cli.bat
+++ upd-cli.bat
@@ -1,12 +1,3 @@
-call npm uninstall -g @angular/cli
-call npm cache verify REM clean -- force
-call npm install -g @angular/cli@latest
-
-rmdir node_modules /s /q
-rmdir dist /s /q
-rmdir tmp /s /q
-
-del package-lock.json /f /q
-
-call npm install --save-dev @angular/cli@latest
-call npm install
+call ng update @angular/cli
+call ng update @angular/core
+call ng update @angular-devkit/build-angular
