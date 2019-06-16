e2633d2f1f31579cc7c204c4a85f67023e67730e
R. Richards <rdrrichards@hotmail.com>
2019-06-16 06:11:54 -0500

    pre update commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,5 +1,8 @@
-CALL npm i -D typescript@3.4.5
+CALL git commit -am "pre update commit"
 CALL ng update @angular/cli
-CALL git commit -m "cli update to latest"
+CALL git commit -am "cli update to latest"
 CALL ng update @angular/core
+CALL git commit -am "ng update to latest"
 CALL npm audit fix
+CALL git commit -am "post update commit"
+CALL build-test.bat
