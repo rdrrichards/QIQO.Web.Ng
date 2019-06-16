b6ec14dec141b64bd6ef7d0cb494c73cb2e9c2b5
R. Richards <rdrrichards@hotmail.com>
2019-06-16 06:16:55 -0500

    gdi

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 CALL git commit -am "pre update commit"
-=======
->>>>>>> a9919e78d987fe8c0770808b78869a798d7a66e7
 CALL ng update @angular/cli
 CALL git commit -am "cli update to latest"
 CALL ng update @angular/core
